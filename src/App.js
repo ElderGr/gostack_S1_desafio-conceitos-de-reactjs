fb4b65128250c3125e794dea47d1fdfea9c7ae0c
ElderGr <gbielrs@gmail.com>
2020-06-18 15:41:25 -0300

    implementado adição de projeto e exclusão conforme especificacões do desafio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from "react";
-
+import React,{ useEffect, useState } from "react";
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() =>{
+      api.get('repositories').then(response => {
+        setRepositories(response.data)
+      })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    
+    const repository = {
+      title: "Desafio Node.js",
+      url: "https://github.com/ElderGr/gostack-conceitos-node",
+      techs: ["Node.js", "Express"]
+    }
+
+    const response = await api.post('repositories', repository)
+
+    setRepositories([ ...repositories, response.data ])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const filteredRepositories = repositories.filter(repository => repository.id !== id)
+
+    api.delete(`repositories/${id}`).then(response => {
+      setRepositories(filteredRepositories);
+    })
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        { repositories.map( repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+          )) 
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
